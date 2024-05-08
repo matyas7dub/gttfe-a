e663fb9664e0af8caa80a3960ced48c44768544d
Matyáš Sedmidubský <matyas-s@seznam.cz>
2024-05-08 18:49:18 +0200

    Fixed routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-// import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import Home from './pages/Home/Home';
+import PageUpdate from './pages/Game/PageUpdate/PageUpdate';
+import Update from './pages/Game/Update/Update';
+import AddToUser from './pages/Role/AddToUser/AddToUser';
 import './App.scss';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}/>
-      <Route path="/game/page-update" element={<Home />}/>
-      <Route path="/game/update" element={<Home />}/>
-      <Route path="/role/add-to-user" element={<Home />}/>
+      <Route path="/game/page-update" element={<PageUpdate />}/>
+      <Route path="/game/update" element={<Update />}/>
+      <Route path="/role/add-to-user" element={<AddToUser />}/>
     </Routes>
   )
 }
---
+++ src/pages/Game/PageUpdate/PageUpdate.tsx
@@ -0,0 +1,5 @@
+export default function PageUpdate() {
+  return (
+    <></>
+  );
+}
