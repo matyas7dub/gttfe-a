7c7d91c2b0dd05bdf76efc2a2e3beb8a20031466
viotalJiplk <61029138+viotalJiplk@users.noreply.github.com>
2024-10-08 10:53:40 +0200

    add permanent storage  (#5)
    * feat(docker): add build selection from .env
    
    * feat(file): add upload and file deletion
    
    * feat(docker): add permanent file storage

---
+++ src/pages/File/List/List.tsx
@@ -0,0 +1,71 @@
+import { Button, Card, Link as ChakraLink, Flex, Stack, useToast } from "@chakra-ui/react";
+import { DeleteIcon } from '@chakra-ui/icons';
+import { useEffect, useState } from "react";
+import Breadcrumbs from "../../../components/Breadcrumbs/Breadcrumbs";
+import { fetchGracefully } from "../../../components/Navbar/Login/LoginScript";
+
+interface ListAPI {
+  fileName: string,
+  address: string
+}
+
+export default function List() {
+  const [files, setFiles] = useState<JSX.Element[]>([<div></div>]);
+
+  const toast = useToast();
+
+  async function deleteFile (filename: string){
+    await fetchGracefully(
+      ((process.env.REACT_APP_PROD === 'yes' ? 'https://gttournament.cz' : process.env.REACT_APP_BACKEND_URL) + "/backend/file/"+ filename),
+      'DELETE',
+      null,
+      [
+        ["Authorization", `Bearer ${localStorage.getItem("jws")}`]
+      ],
+      "Files deleted successfully", toast);
+    listFiles()
+  }
+
+  async function listFiles() {
+    let result = await fetchGracefully(
+      ((process.env.REACT_APP_PROD === 'yes' ? 'https://gttournament.cz' : process.env.REACT_APP_BACKEND_URL) + "/backend/file/list"),
+      'GET',
+      null,
+      [
+        ["Authorization", `Bearer ${localStorage.getItem("jws")}`]
+      ],
+      "Files listed successfully", toast);
+    if (typeof result === undefined) {
+      return
+    }
+    let parsed: ListAPI[] = await result?.json();
+    let tmpFiles = [];
+    console.log(parsed);
+    for (let file of parsed) {
+      tmpFiles.push(<div>
+        <Card width="fit-content" minWidth="30%" marginTop="-2rem">
+          <Flex justifyContent="space-between" alignItems="center"padding="1rem">
+            <ChakraLink href={file.address}>{file.fileName}</ChakraLink>
+            <Button onClick={() => { deleteFile(file.fileName) }}><DeleteIcon></DeleteIcon></Button>
+          </Flex>
+        </Card>
+      </div>)
+    }
+    setFiles(tmpFiles);
+  }
+  useEffect(() => {
+    listFiles();
+  }, []);
+  
+
+  return (
+    <div>
+      <Breadcrumbs />
+
+      <Stack direction="column" spacing="3rem" className="Form">
+        {files}  
+      </Stack>
+    </div>
+  );
+
+}
