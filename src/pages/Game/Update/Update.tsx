e4083e0ace75c91f7274bb2cb201bd374c814cbb
Matyáš Sedmidubský <matyas-s@seznam.cz>
2024-10-10 16:08:25 +0200

    chore(game update): Removed deprecated settings

--- src/pages/Game/Update/Update.tsx
+++ src/pages/Game/Update/Update.tsx
@@ -15,31 +15,19 @@ export default function Update() {
 
   const [regStartEnabled, setRegStartEnabled] = useState(false);
   const [regEndEnabled, setRegEndEnabled] = useState(false);
-  const [maxCaptainsEnabled, setMaxCaptainsEnabled] = useState(false);
-  const [minCaptainsEnabled, setMinCaptainsEnabled] = useState(false);
-  const [maxMembersEnabled, setMaxMembersEnabled] = useState(false);
-  const [minMembersEnabled, setMinMembersEnabled] = useState(false);
-  const [maxReserveEnabled, setMaxReserveEnabled] = useState(false);
-  const [minReserveEnabled, setMinReserveEnabled] = useState(false);
   const [maxTeamsEnabled, setMaxTeamsEnabled] = useState(false);
 
   const [gameId, setGameId] = useState<Number>();
   const [regStart, setRegStart] = useState<String>();
   const [regEnd, setRegEnd] = useState<String>();
-  const [maxCaptains, setMaxCaptains] = useState(1);
-  const [minCaptains, setMinCaptains] = useState(1);
-  const [maxMembers, setMaxMembers] = useState(1);
-  const [minMembers, setMinMembers] = useState(1);
-  const [maxReserve, setMaxReserve] = useState(0);
-  const [minReserve, setMinReserve] = useState(0);
-  const [maxTeams, setMaxTeams] = useState(1);
+  const [maxTeams, setMaxTeams] = useState(0);
 
   return (
     <div>
       <Breadcrumbs />
 
       <Stack direction="column" spacing="3rem" className="Form">
-        <DataPicker dataType={dataType.game} isInvalid={gameErr} changeHandler={(event) => setGame(event.target.value)}/>
+        <DataPicker dataType={dataType.game} isInvalid={gameErr} changeHandler={(event) => selectGame(event.target.value)}/>
 
         <Stack direction="row" spacing={horizontalFormSpacing}>
           <FormControl>
@@ -55,97 +43,17 @@ export default function Update() {
           </FormControl>
         </Stack>
 
-        <Stack direction="row" spacing={horizontalFormSpacing}>
-          <FormControl>
-            <FormLabel>Maximum captains</FormLabel>
-            <Switch marginBottom={inputToggleMargin} isDisabled={gameId == null} isChecked={maxCaptainsEnabled} onChange={(event) => setMaxCaptainsEnabled(event.target.checked)} />
-            <NumberInput min={minCaptainsEnabled ? minCaptains : 0} isDisabled={!maxCaptainsEnabled} value={maxCaptainsEnabled ? maxCaptains : ""} onChange={(_, value) => {setMaxCaptains(value)}}>
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-          </FormControl>
-
-          <FormControl>
-            <FormLabel>Minimum captains</FormLabel>
-            <Switch marginBottom={inputToggleMargin} isDisabled={gameId == null} isChecked={minCaptainsEnabled} onChange={(event) => setMinCaptainsEnabled(event.target.checked)} />
-            <NumberInput min={0} max={maxCaptainsEnabled ? maxCaptains : undefined} isDisabled={!minCaptainsEnabled} value={minCaptainsEnabled ? minCaptains : ""} onChange={(_, value) => {setMinCaptains(value)}}>
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-          </FormControl>
-        </Stack>
-
-        <Stack direction="row" spacing={horizontalFormSpacing}>
-          <FormControl>
-            <FormLabel>Maximum members</FormLabel>
-            <Switch marginBottom={inputToggleMargin} isDisabled={gameId == null} isChecked={maxMembersEnabled} onChange={(event) => setMaxMembersEnabled(event.target.checked)} />
-            <NumberInput min={minMembersEnabled ? minMembers : 0} isDisabled={!maxMembersEnabled} value={maxMembersEnabled ? maxMembers : ""} onChange={(_, value) => {setMaxMembers(value)}}>
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-          </FormControl>
-
-          <FormControl>
-            <FormLabel>Minimum members</FormLabel>
-            <Switch marginBottom={inputToggleMargin} isDisabled={gameId == null} isChecked={minMembersEnabled} onChange={(event) => setMinMembersEnabled(event.target.checked)} />
-            <NumberInput min={0} max={maxMembersEnabled ? maxMembers : undefined} isDisabled={!minMembersEnabled} value={minMembersEnabled ? minMembers : ""} onChange={(_, value) => {setMinMembers(value)}}>
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-          </FormControl>
-        </Stack>
-
-        <Stack direction="row" spacing={horizontalFormSpacing}>
-          <FormControl>
-            <FormLabel>Maximum reserve</FormLabel>
-            <Switch marginBottom={inputToggleMargin} isDisabled={gameId == null} isChecked={maxReserveEnabled} onChange={(event) => setMaxReserveEnabled(event.target.checked)} />
-            <NumberInput min={minReserveEnabled ? minReserve : 0} isDisabled={!maxReserveEnabled} value={maxReserveEnabled ? String(maxReserve) : ""} onChange={(_, value) => {setMaxReserve(value)}}>
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-          </FormControl>
-
-          <FormControl>
-            <FormLabel>Minimum reserve</FormLabel>
-            <Switch marginBottom={inputToggleMargin} isDisabled={gameId == null} isChecked={minReserveEnabled} onChange={(event) => setMinReserveEnabled(event.target.checked)} />
-            <NumberInput min={0} max={maxReserveEnabled ? maxReserve : undefined} isDisabled={!minReserveEnabled} value={minReserveEnabled ? String(minReserve) : ""} onChange={(_, value) => {setMinReserve(value)}}>
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-          </FormControl>
-        </Stack>
-
-        <Stack direction="row" spacing={horizontalFormSpacing}>
-          <FormControl>
-            <FormLabel>Maximum teams</FormLabel>
-            <Switch marginBottom={inputToggleMargin} isDisabled={gameId == null} isChecked={maxTeamsEnabled} onChange={(event) => setMaxTeamsEnabled(event.target.checked)} />
-            <NumberInput min={0} isDisabled={!maxTeamsEnabled} value={maxTeamsEnabled ? String(maxTeams) : ""} onChange={(_, value) => {setMaxTeams(value)}}>
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-          </FormControl>
-        </Stack>
+        <FormControl>
+          <FormLabel>Maximum teams</FormLabel>
+          <Switch marginBottom={inputToggleMargin} isDisabled={gameId == null} isChecked={maxTeamsEnabled} onChange={(event) => setMaxTeamsEnabled(event.target.checked)} />
+          <NumberInput min={0} isDisabled={!maxTeamsEnabled} value={maxTeamsEnabled ? String(maxTeams) : ""} onChange={(_, value) => {setMaxTeams(value)}}>
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+        </FormControl>
 
         <ConfirmationButton isDisabled={gameId == null} onClick={updateGame}>Update game</ConfirmationButton> 
 
@@ -153,18 +61,12 @@ export default function Update() {
     </div>
   );
 
-  function setGame(newGameId: String) {
+  function selectGame(newGameId: String) {
     if (newGameId === "") {
       setGameErr(true);
       setGameId(undefined);
       setRegStartEnabled(false);
       setRegEndEnabled(false);
-      setMaxCaptainsEnabled(false);
-      setMinCaptainsEnabled(false);
-      setMaxMembersEnabled(false);
-      setMinMembersEnabled(false);
-      setMaxReserveEnabled(false);
-      setMinReserveEnabled(false);
       setMaxTeamsEnabled(false);
       return;
     }
@@ -193,60 +95,12 @@ export default function Update() {
         setRegEnd(now);
       }
 
-      if (data.maxCaptains != null) {
-        setMaxCaptains(data.maxCaptains);
-        setMaxCaptainsEnabled(true);
-      } else {
-        setMaxCaptainsEnabled(false);
-        setMaxCaptains(1);
-      }
-
-      if (data.minCaptains != null) {
-        setMinCaptains(data.minCaptains);
-        setMinCaptainsEnabled(true);
-      } else {
-        setMinCaptainsEnabled(false);
-        setMinCaptains(1);
-      }
-
-      if (data.maxMembers != null) {
-        setMaxMembers(data.maxMembers);
-        setMaxMembersEnabled(true);
-      } else {
-        setMaxMembersEnabled(false);
-        setMaxMembers(1);
-      }
-
-      if (data.minMembers != null) {
-        setMinMembers(data.minMembers);
-        setMinMembersEnabled(true);
-      } else {
-        setMinMembersEnabled(false);
-        setMinMembers(1);
-      }
-
-      if (data.maxReservists != null) {
-        setMaxReserve(data.maxReservists);
-        setMaxReserveEnabled(true);
-      } else {
-        setMaxReserveEnabled(false);
-        setMaxReserve(0);
-      }
-
-      if (data.minReservists != null) {
-        setMinReserve(data.minReservists);
-        setMinReserveEnabled(true);
-      } else {
-        setMinReserveEnabled(false);
-        setMinReserve(0);
-      }
-
       if (data.maxTeams != null) {
         setMaxTeams(data.maxTeams);
         setMaxTeamsEnabled(true);
       } else {
         setMaxTeamsEnabled(false);
-        setMaxTeams(1);
+        setMaxTeams(0);
       }
     })
   }
@@ -257,7 +111,7 @@ export default function Update() {
       ["Content-Type", "application/json"]
     ];
 
-    let body = { game_id: gameId };
+    const body = {};
 
     if (regStartEnabled) {
       Object.assign(body, { registrationStart: regStart });
@@ -267,35 +121,11 @@ export default function Update() {
       Object.assign(body, { registrationEnd: regEnd });
     }
 
-    if (maxCaptainsEnabled) {
-      Object.assign(body, { maxCaptains: maxCaptains });
-    }
-
-    if (minCaptainsEnabled) {
-      Object.assign(body, { minCaptains: minCaptains });
-    }
-
-    if (maxMembersEnabled) {
-      Object.assign(body, { maxMembers: maxMembers });
-    }
-
-    if (minMembersEnabled) {
-      Object.assign(body, { minMembers: minMembers });
-    }
-
-    if (maxReserveEnabled) {
-      Object.assign(body, { maxReservists: maxReserve });
-    }
-
-    if (minReserveEnabled) {
-      Object.assign(body, { minReservists: minReserve });
-    }
-
     if (maxTeamsEnabled) {
       Object.assign(body, { maxTeams: maxTeams });
     }
 
-    fetchGracefully(process.env.REACT_APP_BACKEND_URL + '/backend/game/all/',
+    fetchGracefully(process.env.REACT_APP_BACKEND_URL + `/backend/game/${gameId}/`,
     "PUT", JSON.stringify(body), headers, "Game updated successfully", toast);
   }
 }
