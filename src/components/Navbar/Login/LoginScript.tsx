6b55e137576f34c0a6ec6c97983c76ad517dcd64
Matyáš Sedmidubský <matyas-s@seznam.cz>
2024-10-13 18:23:32 +0200

    feat(login): Logging in via new tab
    Now it doesn't lose current page state when logging in

--- src/components/Navbar/Login/LoginScript.tsx
+++ src/components/Navbar/Login/LoginScript.tsx
@@ -1,12 +1,11 @@
 import { Center, CreateToastFnReturn, Spinner, useToast } from "@chakra-ui/react";
 import { useEffect } from "react";
-import { useSearchParams, useNavigate } from "react-router-dom"
+import { useSearchParams } from "react-router-dom"
 import * as jose from 'jose';
 import { loginPath } from "../../../config/config";
 
 export default function LoginScript() {
   let searchParams = useSearchParams();
-  const navigate = useNavigate();
   let code = searchParams[0].get('code');
   let state = searchParams[0].get('state');
   let httpBody = {
@@ -32,29 +31,19 @@ export default function LoginScript() {
         localStorage.setItem("jws", data.jws);
         localStorage.setItem("userObject", JSON.stringify(data.userObject));
       } else {
-        console.error("Couldn't get jws from API:", data.msg);
+        const error = data.msg?? data.message?? "Unknown error";
+        console.error("Couldn't get jws from API:", error);
         toast({
           title: "Couldn't get jws from API",
-          description: data.msg?? data.message?? "Unknown error",
+          description: error,
           status: 'error',
           duration: 5000,
           isClosable: true
         })
       }
-      navigate('/');
+      window.close();
     })
-    .then(() => {
-      if (localStorage.getItem("requestCache") != null) {
-        const cache = JSON.parse(localStorage.getItem("requestCache")?? "this will intentionally crash the parser and it shouldnt ever get here");
-
-        const authorizationIndex = cache.request.headers.flat(1).findIndex((x: string) => x === "Authorization");
-        if (authorizationIndex !== -1) {
-          cache.request.headers[(authorizationIndex + 2)/2 - 1][1] = `Bearer ${localStorage.getItem("jws")}`;
-        }
-
-        fetchWithToast(cache.url, cache.request.method, cache.request.headers, cache.request.body, cache.successMessage, toast)
-      }
-    })
+    .catch(error => console.error("Error", error));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -69,28 +58,30 @@ export function fetchGracefully(url: string, method: string, body: string | null
   // The headers have to be [string, string][] otherwise they get lost at some point
   successMessage = successMessage?? "Success";
 
-  if ((jose.decodeJwt(localStorage.getItem("jws")?? "").exp?? 0) * 1000 < Date.now()) {
-    const cache = {
-      url: url,
-      successMessage: successMessage,
-      request: {
-        method: method,
-        headers: headers,
-        body: body?? undefined
-      },
-    };
-    localStorage.setItem("requestCache", JSON.stringify(cache));
-  
+  if (!localStorage.getItem("jws") || (jose.decodeJwt(localStorage.getItem("jws")?? "").exp?? 0) * 1000 < Date.now()) {
+
     fetch(process.env.REACT_APP_BACKEND_URL + '/backend/discord/auth')
     .then(response => response.json())
-    .then(authUrl => window.location.href = authUrl.redirectUrl + `&redirect_uri=${window.location.origin + loginPath}`)
+    .then(authUrl => window.open(authUrl.redirectUrl + `&redirect_uri=${window.location.origin + loginPath}`, "_blank"))
+    .then(() => {
+      const currentJws = localStorage.getItem("jws");
+      const interval = setInterval(() => {
+        if (currentJws !== localStorage.getItem("jws")) {
+          clearInterval(interval);
+          const newHeaders: [string, string][] = headers;
+          const authIndex = newHeaders.findIndex(value => value[0] === "Authorization");
+          newHeaders[authIndex] = ["Authorization", `Bearer ${localStorage.getItem("jws")}`] as [string, string];
+          fetchWithToast(url, method, newHeaders, body, successMessage, toast);
+        }
+      }, 1000);
+    })
     .catch(error => console.error('Error:', error));
   } else {
     return fetchWithToast(url, method, headers, body, successMessage, toast);
   }
 }
 
-function fetchWithToast(url: string, method: string, headers: [string, string][], body: string | null | any, successMessage: string, toast: CreateToastFnReturn) {
+async function fetchWithToast(url: string, method: string, headers: [string, string][], body: string | null | any, successMessage: string, toast: CreateToastFnReturn) {
   return fetch(url,
     {
       method: method,
