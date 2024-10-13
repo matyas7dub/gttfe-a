6d1d49a9946e0290c38383bb92fb63a4b114dcc4
Matyáš Sedmidubský <matyas-s@seznam.cz>
2024-10-13 17:15:24 +0200

    fix(login): Now correctly displays current login state

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,25 @@
 import { Box, HStack, Img, useColorMode} from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import ColorModeButton from './ColorModeButton/ColorModeButton';
 import Login from './Login/Login';
-import {publicUrl} from '../../config/config';
+import {loginPathRelative, publicUrl} from '../../config/config';
 
 export default function Navbar() {
   const { colorMode, } = useColorMode();
-  return (
-      <HStack position="relative" width="100%" zIndex="10">
-        <Link to="/"><Img src={`${publicUrl}/GtLogo.svg`} filter={colorMode === "light" ? "brightness(0)" : ""} /></Link>
-        <Box flexGrow="1" display="hidden" />
-        <Login />
-        <ColorModeButton />
-      </HStack>
-  );
+  const location = useLocation();
+
+  if (location.pathname === loginPathRelative) {
+    return (
+      <></>
+    );
+  } else {
+    return (
+        <HStack position="relative" width="100%" zIndex="10">
+          <Link to="/"><Img src={`${publicUrl}/GtLogo.svg`} filter={colorMode === "light" ? "brightness(0)" : ""} /></Link>
+          <Box flexGrow="1" display="hidden" />
+          <Login />
+          <ColorModeButton />
+        </HStack>
+    );
+  }
 }
