ad2fec3b3109b6954c9c1ed5f725197700cf8001
Matyáš Sedmidubský <matyas@7dub.dev>
2024-10-29 15:46:01 +0100

    feat(confirmationButton): Added a timeout to the confirmationButton to prevent double clicks

--- src/components/ConfirmationButton/ConfirmationButton.tsx
+++ src/components/ConfirmationButton/ConfirmationButton.tsx
@@ -1,15 +1,24 @@
 import { Button } from "@chakra-ui/react";
-import { MouseEventHandler, PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
 
 type ConfirmationButtonProps = {
   isDisabled?: boolean,
-  onClick: MouseEventHandler
+  onClick: () => void
 }
 export default function ConfirmationButton(props: PropsWithChildren<ConfirmationButtonProps>) {
+  const [wasPushed, setWasPushed] = useState(false);
   return (
-    <Button isDisabled={props.isDisabled} onClick={props.onClick}
+    <Button isDisabled={props.isDisabled || wasPushed} onClick={click}
       fontSize="2rem" colorScheme="GttOrange" width="fit-content" padding="1em">
       {props.children}
     </Button>
   )
+
+  function click() {
+    setWasPushed(true);
+    props.onClick();
+    setTimeout(() => {
+      setWasPushed(false);
+    }, 1500);
+  }
 }
