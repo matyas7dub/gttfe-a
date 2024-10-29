c1fd6300c669653b89d45d15e8e94084c04976a9
Matyáš Sedmidubský <matyas@7dub.dev>
2024-10-29 15:08:16 +0100

    feat(autofill): Finished event autofilling

--- src/pages/Events/AutofillEvent/AutofillEvent.tsx
+++ src/pages/Events/AutofillEvent/AutofillEvent.tsx
@@ -1,8 +1,9 @@
-import { Alert, AlertIcon, AlertTitle, CreateToastFnReturn, FormControl, FormLabel, Input, Stack, Tooltip, useToast } from "@chakra-ui/react";
+import { CreateToastFnReturn, FormControl, FormLabel, Input, Stack, useToast } from "@chakra-ui/react";
 import { useState } from "react";
 import Breadcrumbs from "../../../components/Breadcrumbs/Breadcrumbs";
 import ConfirmationButton from "../../../components/ConfirmationButton/ConfirmationButton";
 import DataPicker, { dataType } from "../../../components/DataPicker/DataPicker";
+import { fetchGracefully } from "../../../components/Navbar/Login/LoginScript";
 import { backendUrl } from "../../../config/config";
 
 export default function AutofillEvent() {
@@ -16,13 +17,6 @@ export default function AutofillEvent() {
       <Breadcrumbs />
 
       <Stack direction="column" spacing="3rem" className="Form">
-        <Tooltip label="As this uses multiple requests, it is not *yet* implemented to automatically relog.">
-          <Alert status="warning">
-            <AlertIcon />
-            <AlertTitle>Make sure you are logged in!</AlertTitle>
-          </Alert>
-        </Tooltip>
-
         <DataPicker dataType={dataType.event} changeHandler={event => selectEvent(Number(event.target.value))} />
 
         <FormControl isDisabled={eventId == null || createdStage}>
@@ -59,7 +53,7 @@ export default function AutofillEvent() {
 
   function createStage() {
     setCreatedStage(true);
-    fetch(backendUrl + "/backend/stage/create",
+    fetchGracefully(backendUrl + "/backend/stage/create",
       {
         method: "POST",
         body: JSON.stringify({
@@ -71,25 +65,12 @@ export default function AutofillEvent() {
           "Authorization": `Bearer ${localStorage.getItem("jws")}`,
           "Content-Type": "application/json"
         }
-      }
-    )
+      },
+      "Created stage successfully", toast)
     .then(async response => {
       if (response.ok) {
-        toast({
-          title: "Stage created successfully",
-          status: 'success',
-          duration: 5000,
-          isClosable: true
-        });
         const stage = await response.json();
         autofillMatches(stage.stageId, teamIds as number[], toast);
-      } else {
-        toast({
-          title: 'Error while creating the stage',
-          status: 'error',
-          duration: 5000,
-          isClosable: true
-        })
       }
     })
     .catch(error => console.error("Error", error));
@@ -97,54 +78,71 @@ export default function AutofillEvent() {
 }
 
 export function autofillMatches(stageId: number, teamIds: number[], toast: CreateToastFnReturn) {
-  if (teamIds == null || teamIds.length === 0) {
-    console.error("Teams array is empty!");
-  } else {
-    let matches: [number, number][] = new Array<[number, number]>();
-    for (let team = 0; team + 1 < teamIds.length; team += 2) {
-      matches.push([teamIds[team], teamIds[team + 1]]);
-    }
+  if (teamIds == null || teamIds.length < 2) {
+    console.error("Not enough teams!");
+    toast({
+      title: "Not enough teams",
+      description: "There isn't enough teams to create matches.",
+      status: "error",
+      duration: 5000,
+      isClosable: true
+    })
+    return;
+  }
 
-    for (let match = 0; match < matches.length; match++) {
-      fetch(backendUrl + "/backend/match/create/",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            stageId: stageId,
-            firstTeamId: matches[match][0],
-            secondTeamId: matches[match][1],
-            firstTeamResult: 0,
-            secondTeamResult: 0
-          }),
-          headers: {
-            "Authorization": `Bearer ${localStorage.getItem("jws")}`,
-            "Content-Type": "application/json"
-          }
+  if (teamIds.length % 2 !== 0) {
+    toast({
+      title: "Odd team count",
+      description: "Some team won't play in the first stage.",
+      status: "warning",
+      duration: 5000,
+      isClosable: true
+    })
+  }
+
+  let matches: [number, number][] = new Array<[number, number]>();
+  for (let team = 0; team + 1 < teamIds.length; team += 2) {
+    matches.push([teamIds[team], teamIds[team + 1]]);
+  }
+
+  for (let match = 0; match < matches.length; match++) {
+    fetchGracefully(backendUrl + "/backend/match/create/",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          stageId: stageId,
+          firstTeamId: matches[match][0],
+          secondTeamId: matches[match][1],
+          firstTeamResult: 0,
+          secondTeamResult: 0
+        }),
+        headers: {
+          "Authorization": `Bearer ${localStorage.getItem("jws")}`,
+          "Content-Type": "application/json"
         }
-      )
-      .then(response => {
-        if (!response.ok) {
-          toast({
-            id: "matchErrorToast",
-            title: 'Error while creating matches',
-            description: `An error occured while creating match (teamId)${matches[match][0]} vs (teamId)${matches[match][1]}`,
-            status: 'error',
-            duration: 5000,
-            isClosable: true
-          })
-        }
-      })
-      .then(() => {
-        if (match === matches.length - 1 && !toast.isActive("matchErrorToast")) {
-          toast({
-            title: "Matches created successfully",
-            status: 'success',
-            duration: 5000,
-            isClosable: true
-          })
-        }
-      })
-      .catch(error => console.error("Error", error));
-    }
+      }, null, toast)
+    .then(response => {
+      if (!response.ok) {
+        toast({
+          id: "matchErrorToast",
+          title: 'Error while creating matches',
+          description: `An error occured while creating match (teamId)${matches[match][0]} vs (teamId)${matches[match][1]}`,
+          status: 'error',
+          duration: 5000,
+          isClosable: true
+        })
+      }
+    })
+    .then(() => {
+      if (match === matches.length - 1 && !toast.isActive("matchErrorToast")) {
+        toast({
+          title: "Matches created successfully",
+          status: 'success',
+          duration: 5000,
+          isClosable: true
+        })
+      }
+    })
+    .catch(error => console.error("Error", error));
   }
 }
