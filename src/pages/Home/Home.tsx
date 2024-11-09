2c21b14e7729f7e3d38316e5b198836727e195d6
Matyáš Sedmidubský <matyas@7dub.dev>
2024-11-09 18:39:04 +0100

    refactor(home): Changed endpoint order

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -16,18 +16,6 @@ import { UsersContent } from '../Users/Users';
 function Home() {
   return (
     <Stack direction="column" minWidth="20em" width="fit-content">
-      <ChakraLink as={RouterLink} to="game"><GroupHeading>Game</GroupHeading></ChakraLink>
-      <GameContent />
-      <FormDivider />
-
-      <ChakraLink as={RouterLink} to="role"><GroupHeading>Role</GroupHeading></ChakraLink>
-      <RoleContent />
-      <FormDivider />
-
-      <ChakraLink as={RouterLink} to="users"><GroupHeading>Users</GroupHeading></ChakraLink>
-      <UsersContent />
-      <FormDivider />
-
       <ChakraLink as={RouterLink} to="events"><GroupHeading>Events</GroupHeading></ChakraLink>
       <EventsContent />
       <FormDivider />
@@ -38,6 +26,18 @@ function Home() {
 
       <ChakraLink as={RouterLink} to="matches"><GroupHeading>Matches</GroupHeading></ChakraLink>
       <MatchesContent />
+      <FormDivider />
+
+      <ChakraLink as={RouterLink} to="game"><GroupHeading>Game</GroupHeading></ChakraLink>
+      <GameContent />
+      <FormDivider />
+
+      <ChakraLink as={RouterLink} to="role"><GroupHeading>Role</GroupHeading></ChakraLink>
+      <RoleContent />
+      <FormDivider />
+
+      <ChakraLink as={RouterLink} to="users"><GroupHeading>Users</GroupHeading></ChakraLink>
+      <UsersContent />
       <FormDivider />
 
       <ChakraLink as={RouterLink} to="file"><GroupHeading>File</GroupHeading></ChakraLink>
