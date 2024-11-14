5adbd0b1d8c5b2d305f70305724021d011c8c199
Matyáš Sedmidubský <matyas@7dub.dev>
2024-11-14 16:57:22 +0100

    refactor(forms): Refactored horizontalFormSpacing to be a global constant

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,3 +2,4 @@ export const publicUrl = process.env.PUBLIC_URL ?? "/"
 export const loginPathRelative = (process.env.REACT_APP_AUTH_REDIRECT ?? '/login')
 export const loginPath = `${publicUrl}${loginPathRelative}`
 export const backendUrl = (process.env.REACT_APP_BACKEND_URL ?? "")
+export const horizontalFormSpacing = "2rem"
