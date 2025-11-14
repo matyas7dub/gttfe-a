6c1abdd026296b00c6f1d787b8998a7cc7bf4dd3
Matyáš Sedmidubský <matyas@7dub.dev>
2025-11-14 08:15:16 +0100

    chore(footer): Remove old guide from footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,6 @@ export default function Footer() {
   const commitHash = String(process.env.REACT_APP_COMMIT_HASH?? "").substring(0, 7);
   return (
     <Center borderTop={`2px solid ${color}`} color={color} gap="2rem" padding="1em" marginTop="20vh" marginBottom="5vh">
-      <a href="https://gttournament.notaku.site/jak-na-gtt-admin-panel" target="_blank" rel="noreferrer">How does this work?</a>
       <Tooltip label={`Built from commit ${commitHash === "" ? "Development" : commitHash}`}>Hover me for build!</Tooltip> </Center>
   )
 }
